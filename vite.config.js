86b14078fc333845205aec372c695dbcc0147641
Ahmed-878 <ahmedabouelhaggago6@gmail.com>
2025-05-02 00:26:32 +0300

    Add module federation & share header component

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    federation({
+      name: "products-app",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Header": "./src/components/Header/Header",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
 });
